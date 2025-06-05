61fd80bab007577d95bce08dcadd22956bf750e2
Mateusz Zajac <matteo.zajac@gmail.com>
2025-06-05 11:06:45 +0200

    Initial Commit

---
+++ BedtimeStories/BedtimeStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BedtimeStoriesApp.swift
+//  BedtimeStories
+//
+//  Created by Mateusz Zajac UR  on 05/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BedtimeStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
